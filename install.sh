a03f2df3fc1317f4b8b5c23a83a440a07da9202c
kazuki <pharcydetip@gmail.com>
2019-08-13 00:12:23 +0900

    add Vundle setup

--- install.sh
+++ install.sh
@@ -2,10 +2,13 @@
 
 for f in .??*
 do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
-    [[ "$f" == ".style.yapf" ]] && continue
+  [[ "$f" == ".git" ]] && continue
+  [[ "$f" == ".DS_Store" ]] && continue
+  [[ "$f" == ".style.yapf" ]] && continue
 
-    echo $f
-    ln -s $HOME/dotfiles/$f $HOME/$f
+  echo $f
+  ln -s $HOME/dotfiles/$f $HOME/$f
 done
+
+# setup Vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
