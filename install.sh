f331602b479204d2f444f83ca9cfdd148ec521cd
kazuki <pharcydetip@gmail.com>
2019-08-14 01:18:16 +0900

    add comments

--- install.sh
+++ install.sh
@@ -35,9 +35,10 @@ fi
 
 # install vim with python interface
 if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-  echo_progress "installing Vim with Python interface..."
+  echo_progress "removing Vim packages..."
   sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
 
+  echo_progress "Vim installing dependency packages..."
   sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev
 
   sudo rm -rf /usr/local/share/vim
@@ -47,6 +48,7 @@ if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   sudo mkdir /usr/include/lua5.1/include
   sudo cp /usr/include/lua5.1/*.h /usr/include/lua5.1/include/
 
+  echo_progress "compiling Vim with Python interface..."
   (cd /opt/ &&
     sudo git clone https://github.com/vim/vim &&
     # sudo git pull && sudo git fetch &&
