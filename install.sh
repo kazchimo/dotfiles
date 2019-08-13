4af59c63ecded84dd8e31c171a3634ee3442a1cf
kazuki <pharcydetip@gmail.com>
2019-08-14 01:05:56 +0900

    modify seperator

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-color_code="\e[33m"
-default_color_code="\e[m"
-window_size=`tput cols`
-window_separator=""
-for i in `seq 1 ${window_size}`
-do
-  window_separator+="="
-done
 
 function echo_progress() {
+  color_code="\e[33m"
+  default_color_code="\e[m"
+  window_size=`tput cols`
+  window_separator="==${$1}"
+  rest_separator_len=$(($window_size-${#window_separator}))
+  for i in `seq 1 ${rest_separator_len}`
+  do
+    window_separator+="="
+  done
+
   echo -e $color_code$window_separator$default_color_code
   echo -e $color_code$1$default_color_code
 }
