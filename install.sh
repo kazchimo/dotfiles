43e6547a04221f2d9f805a879625ec68ab20084c
kazuki <pharcydetip@gmail.com>
2019-08-14 00:45:53 +0900

    echo vim installing progress

--- install.sh
+++ install.sh
@@ -34,6 +34,7 @@ fi
 
 # install vim with python interface
 if [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  echo_progress "installing Vim with Python interface..."
   sudo apt-get remove --purge vim vim-runtime vim-gnome vim-tiny vim-gui-common
 
   sudo apt-get install liblua5.1-dev luajit libluajit-5.1 python-dev ruby-dev libperl-dev libncurses5-dev libatk1.0-dev libx11-dev libxpm-dev libxt-dev
