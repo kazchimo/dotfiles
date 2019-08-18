3d8dff4bc99d29dd03d29ab3bee1b0b14a34cb71
kazuki <pharcydetip@gmail.com>
2019-08-18 22:28:29 +0900

    reate install/git.sh

---
+++ install/git.sh
@@ -0,0 +1,14 @@
+# confirm git info if username or email is not set
+git config --global user.email > /dev/null 2>&1
+if [ ! $? = 0 ]; then
+  echo -n -e "\e[34mgit email?: \e[m"
+  read git_email
+  git config --global user.email $git_email
+fi
+
+git config --global user.name > /dev/null 2>&1
+if [ ! $? = 0 ]; then
+  echo -n -e "\e[34mgit uer name?: \e[m"
+  read git_user_name
+  git config --global user.name $git_user_name
+fi
