d4080120f19ca77e1309add115aa6d20e3402ed4
kazuki <pharcydetip@gmail.com>
2019-08-19 22:07:57 +0900

    install make if not installed

--- install/make.sh
+++ install/make.sh
@@ -1,14 +1,17 @@
-if [ "$(uname)" == 'Darwin' ]; then
-  # Mac
-  :
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-  # Linux
-  sudo apt-get install build-essential
-elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
-  # Cygwin
-  :
-else
-  # other processes
-  echo "Your platform ($(uname -a)) is not supported."
-  exit 1
+if !(type "make" > /dev/null 2>&1); then
+  if [ "$(uname)" == 'Darwin' ]; then
+    # Mac
+    :
+  elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    # Linux
+    sudo apt-get install build-essential
+  elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+    # Cygwin
+    :
+  else
+    # other
+    echo "Your platform ($(uname -a)) is not supported."
+    exit 1
+  fi
+
 fi
