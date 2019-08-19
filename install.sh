f6c1b219d818854a0377fe09d3a0205b720a4851
kazuki <pharcydetip@gmail.com>
2019-08-19 21:33:08 +0900

    use bash command

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-$HOME/dotfiles/install/make.sh
-$HOME/dotfiles/install/vim.sh
-$HOME/dotfiles/install/link_dotfiles.sh
-$HOME/dotfiles/install/git.sh
-$HOME/dotfiles/install/vundle.sh
-$HOME/dotfiles/install/tig.sh
-$HOME/dotfiles/install/zsh.sh
+bash $HOME/dotfiles/install/make.sh
+bash $HOME/dotfiles/install/vim.sh
+bash $HOME/dotfiles/install/link_dotfiles.sh
+bash $HOME/dotfiles/install/git.sh
+bash $HOME/dotfiles/install/vundle.sh
+bash $HOME/dotfiles/install/tig.sh
+bash $HOME/dotfiles/install/zsh.sh
 
 zsh
