7c41d20096e66ab9627d39ec73f4ea3b1f42c984
kazuki <pharcydetip@gmail.com>
2019-08-19 21:45:21 +0900

    fix

--- install/make.sh
+++ install/make.sh
@@ -1,12 +1,12 @@
 if [[ "$uname" == 'Darwin' ]]; then
   # Mac
-elif [[ "$expr substr $uname -s 1 5" == 'Linux' ]]; then
+elif [[ "expr substr $uname -s 1 5" == 'Linux' ]]; then
   # Linux
   sudo apt-get install build-essential
-elif [[ "$expr substr (uname -s 1 10" == 'MINGW32_NT' ]]; then
+elif [[ "expr substr uname -s 1 10" == 'MINGW32_NT' ]]; then
   # Cygwin
 else
   # other
-  echo "Your platform ($(uname -a)) is not supported."
+  echo "Your platform ($uname -a) is not supported."
   exit 1
 fi
