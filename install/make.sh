a82317b8026005cd98befe8d4b94f3058c2f9149
kazuki <pharcydetip@gmail.com>
2019-08-19 21:43:00 +0900

    fix elif syntax error

--- install/make.sh
+++ install/make.sh
@@ -1,12 +1,12 @@
-if [ "$(uname)" == 'Darwin' ]; then
-  # Mac processes
-elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
-  # Linux processes
+if [[ "$uname" == 'Darwin' ]]; then
+  # Mac
+elif [[ "$expr substr $uname -s 1 5" == 'Linux' ]]; then
+  # Linux
   sudo apt-get install build-essential
-elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
-  # Cygwin Processes
+elif [[ "$expr substr (uname -s 1 10" == 'MINGW32_NT' ]]; then
+  # Cygwin
 else
-  # other processes
+  # other
   echo "Your platform ($(uname -a)) is not supported."
   exit 1
 fi
