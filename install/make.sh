130265849b8edf22cc443aa21b29c020f7054a49
kazuki <pharcydetip@gmail.com>
2019-08-19 21:34:44 +0900

    use ==

--- install/make.sh
+++ install/make.sh
@@ -1,9 +1,9 @@
-if [ "$(uname)" = 'Darwin' ]; then
+if [ "$(uname)" == 'Darwin' ]; then
   # Mac processes
-elif [ "$(expr substr $(uname -s) 1 5)" = 'Linux' ]; then
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   # Linux processes
   sudo apt-get install build-essential
-elif [ "$(expr substr $(uname -s) 1 10)" = 'MINGW32_NT' ]; then
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
   # Cygwin Processes
 else
   # other processes
