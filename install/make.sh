a782e5f2a3e717ba56358133e214c6fbddfeb2c5
kazuki <pharcydetip@gmail.com>
2019-08-19 21:49:52 +0900

    use uname

--- install/make.sh
+++ install/make.sh
@@ -1,7 +1,7 @@
-if [[ "$uname" == 'Darwin' ]]; then
+if [[ uname == 'Darwin' ]]; then
   # Mac
   :
-elif [[ "expr substr $uname -s 1 5" == 'Linux' ]]; then
+elif [[ "expr substr uname -s 1 5" == 'Linux' ]]; then
   # Linux
   sudo apt-get install build-essential
 elif [[ "expr substr uname -s 1 10" == 'MINGW32_NT' ]]; then
@@ -9,6 +9,6 @@ elif [[ "expr substr uname -s 1 10" == 'MINGW32_NT' ]]; then
   :
 else
   # other
-  echo "Your platform ($uname -a) is not supported."
+  echo "Your platform (uname -a) is not supported."
   exit 1
 fi
